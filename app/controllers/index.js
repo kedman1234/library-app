58e29ad1c0b7447ceafd84c1340ca1e2dafb6cde
Kedar Kulkarni <k.kulkarni@interproductec.com>
2018-11-20 16:53:20 +0530

    add disable unable button depend on text input empyt or not

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,19 @@
+// import { computed, observer } from '@ember/object';
+import { empty } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    
+    emailAddress: '', // predefine computed property
+    isDisabled: empty('emailAddress'),
+
+    // isDisabled: computed('emailAddress', function() {
+    //     return this.get('emailAddress') === '';
+    // }),
+    // actualEmailAddress: computed('emailAddress', function() {
+    //     console.log("actual email address coputed ", this.get('emailAddress'));
+    // }),
+    // emailAddressChange: observer('emailAddress', function() {
+    //     console.log('observer is called', this.get('emailAddress'));
+    // })
 });
