e75de30798a1cf8a360f5824c7cec6ee49b440f7
Kedar Kulkarni <k.kulkarni@interproductec.com>
2018-11-20 18:17:56 +0530

    validate email field and then set enable disable property for this

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,16 @@
 // import { computed, observer } from '@ember/object';
-import { empty } from '@ember/object/computed';
+// import { empty } from '@ember/object/computed';
+import { match, not } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
     
     emailAddress: '', // predefine computed property
-    isDisabled: empty('emailAddress'),
-
+    // object/computed have some pre defined computed property
+    // empty(), match() are the example of those
+    // isDisabled: empty('emailAddress'), // just check for empty
+    isValid: match('emailAddress', /^.+@.+\..+$/), // do the string contain valid email, it return true/false
+    isDisabled: not('isValid'), // if email is not valid
     // isDisabled: computed('emailAddress', function() {
     //     return this.get('emailAddress') === '';
     // }),
